263ad9129156da56f5cec2898906d801a789802a
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2021-08-18 15:09:56 -0500

    feat: pass branch name to apollo cli (#4)
    * feat: pass branch name to apollo cli
    
    * add author flag and fix the branch flag
    
    * Merge branch 'master' into master
    
    * Update CHANGELOG
    
    Co-authored-by: Ian Sutherland <ian@iansutherland.ca>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,13 @@
 # Apollo Schema Check Action Changelog
 
+## 1.2.0 (August 18, 2021)
+
+- Add branch and author information when pushing to Apollo Studio
+
 ## 1.1.0 (August 18, 2021)
 
 - Add `apolloVersion` setting to specify Apollo CLI version
+- Update Apollo CLI to 2.33.6 (latest)
 - Update dependencies
 
 ## 1.0.0 (July 6, 2020)
--- src/get-arguments.ts
+++ src/get-arguments.ts
@@ -16,7 +16,10 @@ const getArguments = (): string[] => {
     serviceName: getInput('serviceName'),
     validationPeriod: getInput('validationPeriod')
   };
-  const args = [];
+  // see https://www.apollographql.com/docs/studio/github-integration/#github-actions
+  const branch = `--branch=${process.env.GITHUB_REF}#refs/heads/`;
+  const author = `--author=${process.env.GITHUB_ACTOR}`;
+  const args = [branch, author];
 
   debug('token', !!process.env.GITHUB_TOKEN);
 
