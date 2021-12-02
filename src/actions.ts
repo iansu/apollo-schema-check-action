ae8d80935965994080a2b615f7d0526d5220ab92
Ian Sutherland <ian@iansutherland.ca>
2021-12-02 12:32:36 -0700

    Add additional debugging to troubleshoot actions issues (#18)

--- src/actions.ts
+++ src/actions.ts
@@ -2,7 +2,7 @@
 
 import { inspect } from 'util';
 import createDebug from 'debug';
-import { debug as githubDebug } from '@actions/core';
+import { debug as githubDebug, info as githubInfo, error as githubError } from '@actions/core';
 
 const localDebug = createDebug('apollo-schema-check');
 
@@ -18,4 +18,24 @@ const debug = (...args: any[]): void => {
   }
 };
 
-export { isGitHubActions, debug };
+const info = (...args: any[]): void => {
+  if (isGitHubActions()) {
+    for (const arg of args) {
+      githubInfo(inspect(arg));
+    }
+  } else {
+    console.log(args);
+  }
+};
+
+const error = (...args: any[]): void => {
+  if (isGitHubActions()) {
+    for (const arg of args) {
+      githubError(inspect(arg));
+    }
+  } else {
+    console.error(args);
+  }
+};
+
+export { isGitHubActions, debug, info, error };
