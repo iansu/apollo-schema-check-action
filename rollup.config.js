23464ea477a5cba5524a9d25cab2fbdf7aba604f
Ian Sutherland <ian@iansutherland.ca>
2021-12-06 10:27:30 -0700

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
-import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const production = process.env.NODE_ENV === 'production';
 
@@ -13,6 +11,6 @@ export default {
     format: 'cjs',
     exports: 'named',
   },
-  external: ['fs/promises', 'path', 'util', 'vm2'],
-  plugins: [typescript(), json(), commonjs(), nodeResolve(), production && terser()],
+  external: ['fs/promises', 'path', 'util'],
+  plugins: [typescript(), json(), production && terser()],
 };
